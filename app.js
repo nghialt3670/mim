d4595a37ac03e0dd816f7bd97fc80abe8f8f08a9
nghialt3670 <nghialt3670@gmail.com>
2023-12-30 14:33:46 +0700

    update app.js

--- app.js
+++ app.js
@@ -3,37 +3,35 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 
-const MONGODB_CONNECT_URI = ""
-
-// Creating an instance of the Express application.
 const app = express();
 
-// Defining the port number for the server to listen on.
-const PORT = 3000 | process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
-// Connecting to the MongoDB database named 'mim'.
 mongoose.connect(process.env.MONGODB_CONNECT_URI);
 
-// Middleware to parse JSON
+mongoose.connection.on('connected', () => {
+  console.log('Connected to MongoDB');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(`MongoDB connection error: ${err}`);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log('Disconnected from MongoDB');
+});
+
 app.use(express.json());
-
-// Configuring middleware to parse incoming URL-encoded data and populate the req.body object.
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// Configuring session middleware for Express.
 app.use(session({ secret: 'mimapp', resave: false, saveUninitialized: true }));
 
-// Setting the view engine to 'ejs' and specifying the views directory.
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
 
-// Using the 'index' route module for handling requests at the root path.
 app.use('/', require('./routes/index'));
 
-// Starting the Express server and listening on the specified port.
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Serving static files (e.g., CSS, images) from the 'public' directory.
 app.use(express.static('public'));
